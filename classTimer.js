bde41f4f32c00b9e547d7c14bc7f5b87dfe2eb99
Space <audun@nytroe.net>
2016-04-18 19:03:52 +0200

    wrapping timer checks in function

--- classTimer.js
+++ classTimer.js
@@ -3,18 +3,22 @@ if (!Date.now) {
 	Date.now = function() { return new Date().getTime(); }
 }
 
-var currentTime = Math.floor(Date.now() / 1000);
-var timedObjects = document.querySelectorAll("[data-showAfter], [data-hideAfter]");
-
-for (i = 0; i < timedObjects.length; i++) {
-	if (timedObjects[i].hasAttribute("data-showAfter") && timedObjects[i].getAttribute("data-showAfter") <= currentTime) {
-		timedObjects[i].className += " shownAfter";
-	} else {
-		timedObjects[i].className += " hiddenBefore";
-	}
-	if (timedObjects[i].hasAttribute("data-hideAfter") && timedObjects[i].getAttribute("data-hideAfter") <= currentTime) {
-		timedObjects[i].className += " hiddenAfter";
-	} else {
-		timedObjects[i].className += " shownBefore";
+function checkTimers() {
+	var currentTime = Math.floor(Date.now() / 1000);
+	var timedObjects = document.querySelectorAll("[data-showAfter], [data-hideAfter]");
+	
+	for (i = 0; i < timedObjects.length; i++) {
+		if (timedObjects[i].hasAttribute("data-showAfter") && timedObjects[i].getAttribute("data-showAfter") <= currentTime) {
+			timedObjects[i].className += " shownAfter";
+		} else {
+			timedObjects[i].className += " hiddenBefore";
+		}
+		if (timedObjects[i].hasAttribute("data-hideAfter") && timedObjects[i].getAttribute("data-hideAfter") <= currentTime) {
+			timedObjects[i].className += " hiddenAfter";
+		} else {
+			timedObjects[i].className += " shownBefore";
+		}
 	}
 }
+
+checkTimers();
