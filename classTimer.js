e61a09de23cc65abebb794166dd1aa1182f72059
Space <audun@nytroe.net>
2016-04-18 15:09:00 +0200

    initial upload of CodePen version

---
+++ classTimer.js
@@ -0,0 +1,21 @@
+/* IE8 shim polyfill */
+if (!Date.now) {
+	Date.now = function() { return new Date().getTime(); }
+}
+
+var currentTime = Math.floor(Date.now() / 1000);
+var timedObjects = document.querySelectorAll("div");
+
+for (i = 0; i < timedObjects.length; i++) {
+	timedObjects[i].innerHTML = currentTime;
+	if (timedObjects[i].hasAttribute("data-showAfter") && timedObjects[i].getAttribute("data-showAfter") <= currentTime) {
+		timedObjects[i].className += " shownAfter";
+	} else {
+		timedObjects[i].className += " hiddenBefore";
+	}
+	if (timedObjects[i].hasAttribute("data-hideAfter") && timedObjects[i].getAttribute("data-hideAfter") <= currentTime) {
+		timedObjects[i].className += " hiddenAfter";
+	} else {
+		timedObjects[i].className += " shownBefore";
+	}
+}
